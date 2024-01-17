e914db652ba169875f6aa01f2246a2caa12e6b22
Ana Clara Matos <ana.matos@agrisafe.agr.br>
2024-01-17 02:43:41 -0300

    fix: removed unused variable

--- src/components/item-card/index.tsx
+++ src/components/item-card/index.tsx
@@ -4,11 +4,10 @@ type CardProps = {
     photo: string,
     title: string,
     description?: string,
-    type: string,
-    id?: number
+    type: string
 }
 
-export const ItemCard = ({photo, title, description = "", type, id} : CardProps) => {
+export const ItemCard = ({photo, title, description = "", type} : CardProps) => {
     return(
         <Card style={{width: type === 'comic' ? '190px' : '250px'}}>
             <img src={photo} alt={`Image representing ${title}`} />
