d84eb640a7e8e5fa1cc37c3f8bfb5fd8fc02c304
Ana Clara Matos <ana.matos@agrisafe.agr.br>
2024-01-17 00:58:28 -0300

    feat: treat api request errors

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { cookies } from "../../components/cookies-config"
 import { Container } from "../../components/container"
 export const Home = () => {
+    const navigate = useNavigate()
 
+    useEffect(() => {
+        const storedPublicKey = cookies.get('publicKey') || '';
+        const storedPrivateKey = cookies.get('privateKey') || '';
+    
+        if(storedPrivateKey && storedPublicKey){
+            navigate('/characters')
+        }else{
+            navigate('/signin')
+        }
+      }, []);
     return(
         <Container>
-            redirect pra signup ou characters
         </Container>
     )
 }
